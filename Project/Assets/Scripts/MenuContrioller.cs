7655ba87995ba13cd90ad6885b9bd8ec12f02afe
Stephane Tondini <stondini@gmail.com>
2018-12-04 18:00:12 +0100

    Title & Credit panels.

--- Project/Assets/Scripts/MenuContrioller.cs
+++ Project/Assets/Scripts/MenuContrioller.cs
@@ -2,18 +2,36 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MenuContrioller : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public GameObject titlePanel;
+
+    public GameObject creditPanel;
+
+    public Scrollbar creditScrollBar;
+
+    // Use this for initialization
+    void Start () {
+        titlePanel.SetActive(true);
+        creditPanel.SetActive(false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void FixedUpdate()
+    {
+        if (creditScrollBar.value >= 1.0f) 
+        {
+            creditScrollBar.value = 0.0f;
+        }
+
+        creditScrollBar.value = creditScrollBar.value + (0.1f * Time.deltaTime);
+    }
 
     public void OnStart() {
         SceneManager.LoadScene("OutsideScene", LoadSceneMode.Single);
