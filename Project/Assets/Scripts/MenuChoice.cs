26f0be0c5628f5527e0acccec8c376335b4e732c
Stephane Tondini <stondini@gmail.com>
2018-12-08 00:08:36 +0100

    Menu Choice.

---
+++ Project/Assets/Scripts/MenuChoice.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuChoice {
+
+    public static MenuChoice menuChoice = new MenuChoice();
+
+    private int menuIdx;
+
+    public int GetMenuIdx() {
+        return menuIdx;
+    }
+
+    public void SetMenuIdx(int idx) {
+        menuIdx = idx;
+    }
+
+}
